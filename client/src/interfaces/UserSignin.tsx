c4cbab571053b6defcb27525ccb09336b0b0ec57
An-109 <doggod232471@gmail.com>
2024-11-19 22:29:35 -0700

    work

---
+++ client/src/interfaces/UserSignin.tsx
@@ -0,0 +1,8 @@
+
+export interface UserSign {
+    id: number | null;
+    email: string | null;
+    username: string | null;
+    password: string | null;
+  }
+  
---
+++ server/src/models/books.ts
@@ -0,0 +1,58 @@
+import { DataTypes, Sequelize, Model, Optional } from 'sequelize';
+
+ 
+interface BooksData{
+    id: number;
+    name: string;
+    description: string;
+    author: string;
+    genre: string;
+    
+}
+
+interface BookDataCreation extends Optional<BooksData, 'id'>{}
+
+export class Book extends Model<BooksData, BookDataCreation> implements BooksData{
+    
+    public id!: number;
+    public name!: string;
+    public description!: string;
+    public author!: string;
+    public genre!: string;
+
+}
+
+export function BooksMore (sequelize: Sequelize): typeof Book{
+
+    Book.init({
+            id: {
+                type: DataTypes.INTEGER,
+                autoIncrement: true,
+                primaryKey: true,
+              },
+              name: {
+                type: DataTypes.STRING,
+                allowNull: false,
+              },
+              description: {
+                type: DataTypes.STRING,
+                allowNull: false,
+              },
+              author: {
+                type: DataTypes.STRING,
+                allowNull: false,
+              },
+              genre:{
+                type: DataTypes.STRING,
+                allowNull: false
+              }
+            },
+            {
+                tableName: 'books',
+                sequelize,
+                
+              }
+            );
+          
+            return Book;
+        }
