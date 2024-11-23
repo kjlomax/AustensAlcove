f10a0b3a2d7bd1f19e9e41aafbfb085aef4cfa70
Jake <jacob.watson00@yahoo.com>
2024-11-23 16:57:45 -0700

    added navigation bar logic and styling, also pushing assets files

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,50 +1,50 @@
-import { useState, useEffect, useLayoutEffect } from "react";
-import { retrieveUsers } from "../api/userAPI";
-import type { UserData } from "../interfaces/UserData";
-import ErrorPage from "./ErrorPage";
-
-import auth from '../utils/auth';
+// import { useState, useEffect, useLayoutEffect } from "react";
+// import { retrieveUsers } from "../api/userAPI";
+// import type { UserData } from "../interfaces/UserData";
+// import ErrorPage from "./ErrorPage";
+import Navbar from "../components/Navbar";
+// import auth from '../utils/auth';
 
 const Home = () => {
 //does something along the line of confirming and logging in might have put this in the log in page
-    const [users, setUsers] = useState<UserData[]>([]);
-    const [error, setError] = useState(false);
-    const [loginCheck, setLoginCheck] = useState(false);
+    // const [users, setUsers] = useState<UserData[]>([]);
+    // const [error, setError] = useState(false);
+    // const [loginCheck, setLoginCheck] = useState(false);
 
-    useEffect(() => {
-        if (loginCheck) {
-            fetchUsers();
-        }
-    }, [loginCheck]);
+    // useEffect(() => {
+    //     if (loginCheck) {
+    //         fetchUsers();
+    //     }
+    // }, [loginCheck]);
 
-    useLayoutEffect(() => {
-        checkLogin();
-    }, []);
+    // useLayoutEffect(() => {
+    //     checkLogin();
+    // }, []);
 
-    const checkLogin = () => {
-        if (auth.loggedIn()) {
-            setLoginCheck(true);
-        }
-    };
+    // const checkLogin = () => {
+    //     if (auth.loggedIn()) {
+    //         setLoginCheck(true);
+    //     }
+    // };
 
-    const fetchUsers = async () => {
-        try {
-            const data = await retrieveUsers();
-            setUsers(data)
-        } catch (err) {
-            console.error('Failed to retrieve tickets:', err);
-            setError(true);
-        }
-    }
+    // const fetchUsers = async () => {
+    //     try {
+    //         const data = await retrieveUsers();
+    //         setUsers(data)
+    //     } catch (err) {
+    //         console.error('Failed to retrieve tickets:', err);
+    //         setError(true);
+    //     }
+    // }
 
-    if (error) {
-        return <ErrorPage />;
-    }
+    // if (error) {
+    //     return <ErrorPage />;
+    // }
 
     return (
-        <>
-            {/* todo add the home page itself design*/}
-        </>
+        <div>
+            <h1></h1>
+        </div>
     );
 };
 
