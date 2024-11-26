d10dc8b87b88da16af388d853a7de8d5455d6cf0
Jake <jacob.watson00@yahoo.com>
2024-11-25 20:39:27 -0700

    added carousel functionality

---
+++ client/src/components/Button.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../styles/Button.css'; // Import the styles
+
+interface ButtonProps {
+  text: string;
+  onClick: () => void;
+  variant?: 'left' | 'right'; // Optional prop to choose button style (left or right)
+}
+
+const Button = ({ text, onClick, variant }: ButtonProps) => {
+  return (
+    <button className={`btn ${variant}`} onClick={onClick}>
+      {text}
+    </button>
+  );
+};
+
+export default Button;
