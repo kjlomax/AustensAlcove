93e643040c11c3331fa12d002c8dffd1047714a0
Keaton007 <keatonjonesy@gmail.com>
2024-11-26 22:18:19 -0700

    Connection to the Book API

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -8,6 +8,8 @@ import SignIn from './pages/SignUp.tsx';
 import ErrorPage from './pages/ErrorPage.tsx';
 import Home from './pages/Home.tsx';
 import Login from './pages/Login.tsx';
+import SearchLibrary from './pages/SearchLibrary.tsx';
+
 //todo add pages and path
 const router = createBrowserRouter([
   {
@@ -23,9 +25,13 @@ const router = createBrowserRouter([
         path: '/login',
         element: <Login />,
       },
+      // {
+      //   path: '/profile',
+      //   element: <ViewProfile />,
+      // },
       {
-        path: '/profile',
-        element: <ViewProfile />,
+        path: '/search-library',
+        element: <SearchLibrary/>,
       },
       {
         path: 'SignUp',
