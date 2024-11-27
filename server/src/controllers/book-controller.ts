cc264a295596754683475b590af372c2d89f53e0
Jake <jacob.watson00@yahoo.com>
2024-11-26 23:15:17 -0700

    added book-controller and streamlined controllers and routes, added assignedUser to the book model

---
+++ server/src/controllers/book-controller.ts
@@ -0,0 +1,58 @@
+
+import type { Request, Response } from 'express';
+import { Books, User } from '../models/index.js';
+
+export const getAllBooks = async (_req: Request, res: Response) => {
+    try {
+        const books = await Books.findAll({
+          include: [
+            {
+              model: User,
+              as: 'assignedUser',
+              attributes: ['username']
+            },
+          
+          ]
+        });
+        res.status(200).json(books);
+      } catch (error: any) {
+        console.log(error);
+        res.status(500).json({
+          message: error.message
+        });
+      }
+};
+
+export const getBookId = async (req: Request, res: Response) =>{
+    const {id} = req.params;
+    try {
+        const book = await Books.findByPk(id,{attributes:{exclude: ['password']},
+        });
+        if (book) {
+            res.json(book);
+        } else {
+            res.status(400).json({message: 'book not found'})
+        }
+        res.json(book)
+    } catch (error: any) {
+        res.status(500).json({message: error.message});
+    }
+}
+
+  // DELETE /Users/:id
+export const deleteBook = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    try {
+      const book = await Books.findByPk(id);
+      if (book) {
+        await book.destroy();
+        res.json({ message: 'Book deleted' });
+      } else {
+        res.status(404).json({ message: 'Book not found' });
+      }
+      res.json({message:`Book ${id} deleted successfully`})
+    } catch (error: any) {
+      res.status(500).json({ message: error.message });
+    }
+  };
+  
--- server/src/routes/api/book-routes.ts
+++ server/src/routes/api/book-routes.ts
@@ -1,112 +1,11 @@
 import express from 'express';
-import type { Request, Response } from 'express';
-import { Books, User } from '../../models/index.js';
+import { getAllBooks, getBookId, deleteBook } from '../../controllers/book-controller.js';
 
 
 const router = express.Router();
-// GET books
-router.get('/', async (_req: Request, res: Response) => {
-    try {
-      const books = await Books.findAll({
-        include: [
-          {
-            model: User,
-            as: 'assignedUser',
-            attributes: ['username']
-          },
-        
-        ]
-      });
-      res.status(200).json(books);
-    } catch (error: any) {
-      console.log(error);
-      res.status(500).json({
-        message: error.message
-      });
-    }
-  });
-  
-  // // GET /volunteers/:id - Get a volunteer by ID
-  // router.get('/:id', async (req: Request, res: Response) => {
-  //   const { id } = req.params;
-  //   try {
-  //     // const volunteer = await Volunteer.findByPk(id);
-  //     // if(volunteer) {
-  //     //   res.json(volunteer);
-  //     // } else {
-  //     //   res.status(404).json({
-  //     //     message: 'Volunteer not found'
-  //     //   });
-  //     // }
-  //     res.status(200).json('getOneBook');
-  //   } catch (error: any) {
-  //     console.log(error);
-  //     res.status(500).json({
-  //       message: error.message
-  //     });
-  //   }
-  // });
-  
-  // // POST /volunteers - Create a new volunteer
-  // router.post('/', async (req: Request, res: Response) => {
-  //   const { volunteerName } = req.body;
-  //   try {
-  //     // const newVolunteer = await Volunteer.create({
-  //     //   volunteerName
-  //     // });
-  //     res.status(201).json('addBook');
-  //   } catch (error: any) {
-  //     console.log(error);
-  //     res.status(400).json({
-  //       message: error.message
-  //     });
-  //   }
-  // });
-  
-  // // PUT /volunteers/:id - Update a volunteer by ID
-  // router.put('/:id', async (req: Request, res: Response) => {
-  //   const { id } = req.params;
-  //   const { volunteerName } = req.body;
-  //   try {
-  //     // const volunteer = await Volunteer.findByPk(id);
-  //     // if(volunteer) {
-  //     //   volunteer.volunteerName = volunteerName;
-  //     //   await volunteer.save();
-  //     //   res.json(volunteer);
-  //     // } else {
-  //     //   res.status(404).json({
-  //     //     message: 'Volunteer not found'
-  //     //   });
-  //     // }
-  //     res.status(200).json('updateBook');
-  //   } catch (error: any) {
-  //     console.log(error);
-  //     res.status(400).json({
-  //       message: error.message
-  //     });
-  //   }
-  // });
-  
-  // // DELETE /volunteers/:id - Delete a volunteer by ID
-  // router.delete('/:id', async (req: Request, res: Response) => {
-  //   const { id } = req.params;
-  //   try {
-  //     // const volunteer = await Volunteer.findByPk(id);
-  //     // if(volunteer) {
-  //     //   await volunteer.destroy();
-  //     //   res.json({ message: 'User deleted' });
-  //     // } else {
-  //     //   res.status(404).json({
-  //     //     message: 'User not found'
-  //     //   });
-  //     // }
-  //     res.status(200).json('deleteBook');
-  //   } catch (error: any) {
-  //     console.log(error);
-  //     res.status(500).json({
-  //       message: error.message
-  //     });
-  //   }
-  // });
+router.get('/', getAllBooks);
+router.get('/:id', getBookId);
+router.delete('/:id', deleteBook);
+
 
 export { router as bookRouter };
