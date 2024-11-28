2e6e17cc8d6e7be5afe17dc756bcc25f5695e295
Jake <jacob.watson00@yahoo.com>
2024-11-27 20:19:31 -0700

    added favorites model

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -2,11 +2,15 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 import { BooksMore } from './books.js';
 import { UserMore } from './user.js'
+import { FavoritesMore } from './favorites.js'
 import sequelize from '../config/connections.js'
 
 const Books = BooksMore(sequelize);
 const User = UserMore(sequelize);
+const Favorites = FavoritesMore(sequelize);
 
-User.hasMany(Books, { foreignKey: 'assignedUserId' })
+User.hasMany(Books, { foreignKey: 'assignedUserId' });
+User.hasMany(Favorites, { foreignKey: 'userId' });
 Books.belongsTo(User, { foreignKey: "assignedUserId", as: 'assignedUser' })
+
 export { Books, User, sequelize };
